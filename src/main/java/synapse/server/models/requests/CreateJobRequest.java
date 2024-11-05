abcb059395259c4ccd14692a352743176b6e5c34
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-11-05 15:41:07 +0530

    Update CreateJobRequest.java

--- src/main/java/synapse/server/models/requests/CreateJobRequest.java
+++ src/main/java/synapse/server/models/requests/CreateJobRequest.java
@@ -4,11 +4,29 @@ public class CreateJobRequest {
     String type;
     String payload;
     String data;
+    String clientID;
+    String jobLeaderID;
 
     public CreateJobRequest(String type, String payload, String data) {
         this.type = type;
         this.payload = payload;
         this.data = data;
+    }
+
+    public String getJobLeaderID() {
+        return jobLeaderID;
+    }
+
+    public void setJobLeaderID(String jobLeaderID) {
+        this.jobLeaderID = jobLeaderID;
+    }
+
+    public String getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(String clientID) {
+        this.clientID = clientID;
     }
 
     public String getType() {
